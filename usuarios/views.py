a6e1fda061799c14aac422443fc6d62dddacb849
William Santana <williamandrade1058@gmail.com>
2024-07-14 12:42:06 -0300

    modify urls, views of usuarios and add templates login, cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def login(request):
+    return render(request, "usuarios/login.html")
+
+
+def cadastro(request):
+    return render(request, "usuarios/cadastro.html")
+
+
+# def logout(request):
+#     pass
