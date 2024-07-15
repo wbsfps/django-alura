f61d0f983e542992852232d39c78859764a05f5c
William Santana <williamandrade1058@gmail.com>
2024-07-15 18:36:17 -0300

    estilizando formulário

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,16 +2,25 @@ from django import forms
 
 
 class LoginForms(forms.Form):
-    nome = forms.CharField(
+    nome_login = forms.CharField(
         label="Nome de login",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: William Santana"
+            }
+        )
     )
     senha = forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
         widget=forms.PasswordInput(
-
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
         )
     )
