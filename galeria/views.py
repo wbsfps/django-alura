d37420ab6b317cc0125a6912b2febf0e16fb455e
William Santana <williamandrade1058@gmail.com>
2024-07-21 09:24:08 -0300

    reorganizando diretório

--- galeria/views.py
+++ galeria/views.py
@@ -12,12 +12,12 @@ def index(request):
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(
         publicada=True
     )
-    return render(request, 'galeria/index.html', {"cards": fotografias})
+    return render(request, 'index.html', {"cards": fotografias})
 
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+    return render(request, 'imagem.html', {"fotografia": fotografia})
 
 
 def buscar(request):
@@ -34,4 +34,4 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
+    return render(request, "buscar.html", {"cards": fotografias})
